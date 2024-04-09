e0f8c7d6fac367d7a89a50945283ab9b0df5decf
louismiranda <louismiranda@10.2.0.249>
2024-04-08 23:34:48 -0400

    Louis Miranda - I added a print statement saying "Hi! This is Louis Miranda :)"

---
+++ src/HelloWorldMain.java
@@ -0,0 +1,13 @@
+
+
+public class HelloWorldMain {
+
+    public static void main (String [] args){
+
+        System.out.println("Hello World");
+        
+        //Added Change by Louis Miranda
+        System.out.println("Hi! This is Louis Miranda :)");
+    }
+
+}
